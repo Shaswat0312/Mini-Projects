eec734217cc69f14d518191f822e7716b1db3dba
Shaswat Mishra <mishra3shaswat@gmail.com>
2025-07-24 10:40:21 +0530

    Weather App

---
+++ Script.js
@@ -0,0 +1,23 @@
+const apikey = "c5a3bf33bf78a3b7d75a45529598b42c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+const cit = document.querySelector(".City");
+const temp = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".Wind");
+const btn = document.querySelector(".btn");
+const val =  document.querySelector(".box")
+
+async function checkfunction(cityName){
+    const response = await fetch(apiUrl + `&APPID=${apikey}` +`&q=${cityName}` );
+    let data = await response.json()
+
+    cit.innerHTML = data.name;
+    temp.innerHTML = data.main.temp;
+     humidity.innerHTML = data.main.humidity;
+    wind.innerHTML = data.wind.speed;
+
+}
+
+
+btn.addEventListener('click',() =>{checkfunction(val.value)})
